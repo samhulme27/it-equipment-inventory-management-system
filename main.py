eb69beeff3163ab7fd5fa9c4897b648183c2d0bc
samhulme27 <hulme1905@outlook.com>
2026-05-03 18:43:03 +0100

    Added mapping for the excel file

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import pandas as pd 
 
 
+EXL_FILE = "inventory.xlsx"
+
+COL_DEVICE_NAME = "Device Name"
+COL_DEVICE_TYPE = "Device Type"
+COL_USER = "User"
+COL_LOCATION = "location"
+COL_DATE = "Date"
+COL_NOTES = "Notes"
+COL_STATUS = "Status"
+
+# Mapping for the excel file and excel table colums, will need to change later when I've made the proper excel document 
+
+
 df = pd.read_excel("inventory.xlsx") 
 # load excel file into a table in python and call it df
 print(df)
