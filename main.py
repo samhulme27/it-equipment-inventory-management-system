959d3f24832d690cc64d5fe0899ac9ebd305eadb
samhulme27 <hulme1905@outlook.com>
2026-05-03 19:43:26 +0100

    added function for adding a new row

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import datetime
 
 
 EXL_FILE = "inventory.xlsx"
@@ -25,3 +26,29 @@ def save_data(df):
     df.to_excel(EXL_FILE, index=False)
 
 # Function save data, takes updated python table data and writes it back into excel
+
+
+def add_new_device(device_name, device_type, user, location, date, notes, status):
+
+    df = load_data()
+    #Load the data
+    date = datetime.datetime.now()
+    # get the date and time now
+    
+    new_row = {
+        COL_DEVICE_NAME: device_name,
+        COL_DEVICE_TYPE: device_type,
+        COL_USER: user,
+        COL_LOCATION: location,
+        COL_DATE: date,
+        COL_NOTES: notes,
+        COL_STATUS: status
+
+        }
+
+    new_df = pd.DataFrame([new_row])
+    df = pd.concat([df, new_df], ignore_index=True)
+
+    #get the data from the new row, merge the new row with the df
+    save_data(df)
+
